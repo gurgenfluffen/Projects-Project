5d4d0811d2c1d2f9881e1556ed2b70221860ee6e
gurgenfluffen <kinnaman.erika@gmail.com>
2024-02-24 09:17:06 -0800

    first project commit

--- mysql-java/src/main/java/projects/dao/DbConnection.java
+++ mysql-java/src/main/java/projects/dao/DbConnection.java
@@ -7,23 +7,22 @@ import projects.exception.DbException;
 
 public class DbConnection {
 
-	private static String HOST = "local host";
-	private static String PASSWORD = "projects";
-	private static int PORT = 3306;
-	private static String SCHEMA = "projects";
-	private static String USER = "projects";
-	
+	private static final String HOST = "localhost";
+	private static final String PASSWORD = "projects";
+	private static final int PORT = 3306;
+	private static final String SCHEMA = "projects";
+	private static final String USER = "projects";
+
 	public static Connection getConnection() {
-		String uri = String.format("jdbc:mysql://local host:3306/projects?user=projects&password=projects",
-				HOST, PORT, SCHEMA, USER, PASSWORD);
-		
+		String url = String.format("jdbc:mysql://%s:%d/%s?user=%s&password=%s&useSSL=false", HOST, PORT, SCHEMA, USER, PASSWORD);
+
 		try {
-			Connection con = DriverManager.getConnection(uri);
+			Connection con = DriverManager.getConnection(url);
 			System.out.println("Connection to " + SCHEMA + " is successful.");
 			return con;
 		} catch (SQLException e) {
-			System.out.println("Unable to connect at " + uri);
-			throw new DbException ("Unable to connect at " + uri);
+			System.out.println("Unable to connect at " + url);
+			throw new DbException("Unable to connect at " + url);
 		}
 	}
 }
